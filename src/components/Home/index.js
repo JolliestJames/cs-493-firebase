0b8213dd1cc456a69b2991a5fd2fc589698fd0a9
JolliestJames <cloudismydog@gmail.com>
2019-01-07 20:01:16 -0800

    Add Home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        User Details
+      </div>
+    );
+  }
+}
+
+export default Home;
