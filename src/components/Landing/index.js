bbc719167a3bf678e8c745d2eaeeaacf5163e97f
JolliestJames <cloudismydog@gmail.com>
2019-01-07 20:00:41 -0800

    Add Landing component

---
+++ src/components/Landing/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+
+      </form>
+    );
+  }
+}
+
+export default Landing;
