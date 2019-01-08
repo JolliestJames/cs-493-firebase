582eb9097e14008274dc35b59e60a3bc9a464058
JolliestJames <cloudismydog@gmail.com>
2019-01-08 00:52:44 -0800

    Update Landing component, add simple sign up/sign in form

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react';
+import { withFirebase } from '../Firebase';
 
-class Landing extends Component {
+class LandingBase extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  onSubmit = event => {
+    const { email, password } = this.state;
+
+    this.props.firebase
+      .createUser(email, password)
+      .then(authUser => {
+        this.setState({ email: '', password: '' });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+
+    event.preventDefault();
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
-    return (
-      <form onSubmit={this.onSubmit}>
+    const {email, password} = this.state;
 
-      </form>
+    return (
+      <div>
+        <input
+          name="email"
+          value={email}
+          type="text"
+          onChange={this.onChange}
+          placeholder="Email"
+        />
+        <input
+          name="password"
+          value={password}
+          type="password"
+          onChange={this.onChange}
+          placeholder="Password"
+        />
+        <form>
+          <button type="submit">Sign In</button>
+        </form>
+        <form onSubmit={this.onSubmit}>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
     );
   }
 }
+
+const Landing = withFirebase(LandingBase);
 
 export default Landing;
