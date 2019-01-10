c880b746aa980ed2012a2b162c6c9ec12e1e0808
JolliestJames <cloudismydog@gmail.com>
2019-01-09 17:21:32 -0800

    Update Home component styling

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,11 +2,38 @@ import React, { Component } from 'react';
 
 class Home extends Component {
   render() {
+    const {container, content} = styles;
+
     return (
-      <div>
-        User Details
+      <div style={container}>
+        <div style={content}>
+          User Details
+        </div>
       </div>
     );
+  }
+}
+
+const styles = {
+  content: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    width: '60%',
+    height: '10rem',
+    padding: '1rem',
+    margin: '0 auto'
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000',
+    width: '80%',
+    height: '15rem',
+    padding: '1rem',
+    margin: '0 auto'
   }
 }
 
