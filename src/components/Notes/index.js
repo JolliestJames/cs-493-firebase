891fa11d80d2ce83341350f4167c727e92033885
JolliestJames <cloudismydog@gmail.com>
2019-01-12 15:18:57 -0800

    Build out Notes component

--- src/components/Notes/index.js
+++ src/components/Notes/index.js
@@ -6,10 +6,11 @@ import * as ROUTES from '../../constants/routes';
 class Notes extends Component {
   constructor(props) {
     super(props);
-
+    
     this.state = {
       title: '',
-      body: ''
+      body: '',
+      notes: []
     }
   }
 
@@ -23,14 +24,77 @@ class Notes extends Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  viewNote(note_id, event) {
+    console.log(note_id)
+
+    this.props.history.push(
+      {
+        pathname: ROUTES.VIEW_NOTE,
+        state: { note_id: note_id }
+      }
+    );
+
+    event.preventDefault();
+  }
+
   render() {
+    const {notes} = this.state;
+    const {notes_div, content} = styles;
+
+    this.props.firebase.getNotes().once('value').then(snapshot => {
+      this.setState({
+        notes: snapshot
+      });
+    });
+
+    var notes_data = []
+    
+    notes.forEach(note => {
+      notes_data.push({
+        id: note.key,
+        title: note.val().title,
+        body: note.val().body
+      })
+    });
+
     return (
-      <div>
+      <div style={content}>
+        <h1 style={notes_div}>Notes</h1>
+        { 
+          notes_data.map(note => 
+            <div key={note.id} style={notes_div}>
+              <label>Title: </label>
+              <span onClick={this.viewNote.bind(this, note.id)}>
+                {note.title}
+              </span>
+              <br/><br/>
+            </div>
+          )
+        }
         <form onSubmit={this.createNote}>
           <button>Create Note</button>
         </form>
       </div>
     );
+  }
+}
+
+const styles = {
+  notes_div: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  content: {
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    width: '30rem',
+    height: '60%',
+    padding: '1rem',
+    margin: '0 auto'
   }
 }
 
