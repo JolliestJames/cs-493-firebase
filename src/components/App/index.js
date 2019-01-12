4cf0114fd49e992b67b217208abac8b455a86385
JolliestJames <cloudismydog@gmail.com>
2019-01-11 23:21:59 -0800

    Update routing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from '../Navigation';
 import Landing from '../Landing';
 import Home from '../Home';
+import Notes from '../Notes';
+import CreateNote from '../CreateNote';
 
 import * as ROUTES from '../../constants/routes'
 
@@ -16,6 +18,8 @@ const App = () => (
 
       <Route exact path={ROUTES.LANDING} component={Landing} />
       <Route path={ROUTES.HOME} component={Home} />
+      <Route path={ROUTES.NOTES} component={Notes} />
+      <Route path={ROUTES.CREATE_NOTE} component={CreateNote} />
     </div>
   </Router>
 );
