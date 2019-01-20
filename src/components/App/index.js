138589abd6114b6792d785d5d2a367b234194751
JolliestJames <cloudismydog@gmail.com>
2019-01-20 12:12:03 -0800

    Update routing for new category components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,6 +7,9 @@ import Home from '../Home';
 import Notes from '../Notes';
 import Note from '../Note';
 import CreateNote from '../CreateNote';
+import Categories from '../Categories';
+import CreateCategory from '../CreateCategory';
+import Category from '../Category';
 
 import * as ROUTES from '../../constants/routes'
 
@@ -24,6 +27,9 @@ const App = () => (
       <Route path={ROUTES.NOTES} component={Notes} />
       <Route path={ROUTES.CREATE_NOTE} component={CreateNote} />
       <Route path={ROUTES.VIEW_NOTE} component={Note} />
+      <Route path={ROUTES.CREATE_CATEGORY} component={CreateCategory} />
+      <Route path={ROUTES.CATEGORIES} component={Categories} />
+      <Route path={ROUTES.VIEW_CATEGORY} component={Category} />
     </div>
   </Router>
 );
