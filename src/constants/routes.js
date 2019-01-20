d7a97195ddbf9e6031dbf64495b20998e333e392
JolliestJames <cloudismydog@gmail.com>
2019-01-20 12:09:20 -0800

    Add routing for viewing all categories, viewing a single category, and creating a category

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,3 +3,6 @@ export const LANDING = '/';
 export const NOTES = '/notes';
 export const CREATE_NOTE = '/create_note';
 export const VIEW_NOTE = '/view_note';
+export const CATEGORIES = '/categories';
+export const CREATE_CATEGORY = '/create_category';
+export const VIEW_CATEGORY = '/view_category';
